1c2494ede4ea0f349d2807139603b3e1343c6af8
Olga Parfenova <work.parfenova@gmail.com>
2023-03-01 18:01:30 +0300

    The first module is completed

---
+++ script.js
@@ -0,0 +1,86 @@
+// task 1
+
+// let str = 'js';
+// console.log(str.toUpperCase());
+
+// task 2
+
+// let str = 'JS';
+// console.log(str.toLowerCase());
+
+// task 3
+
+// let str = 'я учу javascript!';
+//
+// let method1 = str.substr(2, 3);
+// let method2 = str.substring(2, 6);
+// let method3 = str.slice(2, 6);
+//
+// let m1w2 = str.substr(6, 10);
+// let m2w2 = str.substring(6, 16);
+// let m3w2 = str.slice(6, -1);
+
+// task 4
+
+// let str = 'abcde';
+// console.log(str.indexOf('c'));
+
+// task 5
+
+// let str = 'ovnovur';
+// console.log(str.indexOf('a'));
+
+// task 6
+
+// let str = 'afrgwth';
+// if (str.indexOf('a') === 0) console.log('Начинается с "a"');
+
+// task 7
+
+// let str = 'vfwvgqa';
+// if(str.lastIndexOf('a') === 6) console.log('Заканчивается на "а"');
+
+// task 8
+
+// let str = 'http://wkuierqui';
+// if (str.indexOf('http://') === 0) {
+//     console.log('yes');
+// } else {
+//     console.log('no');
+// }
+
+// task 9
+
+// let str = 'rejng';
+// if (str.indexOf('.html') === 5) {
+//     console.log('yes');
+// } else {
+//     console.log('no');
+// }
+
+// task 10
+
+// let str = 'efwgw4rg';
+// console.log(str.startsWith('http://'));
+
+// task 11
+
+// let str = 'avregwr.html';
+// console.log(str.endsWith('.html'));
+
+// task 12
+
+// let str = '1-2-3-4-5';
+// let arr = str.split('-');
+// console.log(arr);
+
+// task 13
+
+// let str = '12345';
+// let arr = str.split('');
+// console.log(arr);
+
+// task 14
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr.join('-'));
